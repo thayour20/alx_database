1a600c5d65afbe2162ddd9a777e792eb0f0da110
thayour20 <suleimanridwan@gmail.com>
2023-10-10 17:28:03 +0100

    PRI

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- statement to create a table with primary KEY
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO GENERATED,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     UNIQUE KEY (id),
     PRIMARY KEY (id)
